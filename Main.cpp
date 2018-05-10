8e45006949a1da435a99ef5379545b2146a56a49
SohanCSERU <mdsohanurrahman63@gmail.com>
2018-05-10 10:07:48 +0600

    Main Function Written

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "sum.h"
+#include "subtract.h"
+#include "Division.h"
+#include "multi.h"
+using namespace std;
+
+int main()
+{
+	int option, op1, op2;
+	cout << "What do you want to do?\n";
+	cout << "1. Add\n";
+	cout << "2. Subtract\n";
+	cout << "3. Multiply\n";
+	cout << "4. Division\n";
+	
+	return 0;
+}
